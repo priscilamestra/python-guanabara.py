0e5371c9e913d5cd008f45933e4ba47db0463589
priscilamestra <priscilamestra@hotmail.com>
2026-04-05 17:56:23 -0300

    Formatação atualizada dos exercícios

--- exe15.py
+++ exe15.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 print("Aluguel de carros")
 dias = int(input("Quanto dias alugados? "))
-km = float(input("Quantods km rodados? "))
+km = float(input("Quantos km rodados? "))
 preço = (dias * 60) + (km * 0.15)
 
-print("Preço total do aluguel: R${:.2f} " .format(preço))
+print(f"Preço total do aluguel: R${preço:.2f}")
