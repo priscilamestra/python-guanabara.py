c12a72952ef02887647c574baa9b7344d37c972d
priscilamestra <priscilamestra@hotmail.com>
2026-04-05 17:47:40 -0300

    Formatação atualizada dos exercícios

--- exe14.py
+++ exe14.py
@@ -5,6 +5,5 @@ fahrenheit = float(input("Informe a temperatura em °F:"))
 f = celsius * 1.8 + 32
 c = (fahrenheit - celsius) / 1.8
 
-
-print("A temperatura de {:.2f}°C corresponde a {:.2f}°F" .format(celsius,f))
-print("A temperatura de {:.2f}°F corresponde a {:.2f}°C" .format(fahrenheit,c))
+print(f"A temperatura de {celsius:.2f}°C corresponde a {f:.2f}°F")
+print(f"A temperatura de {fahrenheit:.2f}°F corresponde a {c:.2f}°C")
