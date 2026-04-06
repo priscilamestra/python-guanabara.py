ff6f1d155aefe866df2efdbf9697fca99da193c1
priscilamestra <priscilamestra@hotmail.com>
2026-04-06 18:56:42 -0300

    V2

--- exe49.py
+++ exe49.py
@@ -1,7 +1,15 @@
 print("Tabuada v.2")
 
-n = int(input("Digite um número para ver sua tabuada:"))
+n = int(input("Digite um número para ver sua tabuada: "))
 for c in range(1,11):
     resultado = n * c 
-    print("{} x {} = {}." .format(n,c,resultado))
+    print(f"{n} x {c:2} = {resultado}")
     # ou faz a multiplicação direta dentro do format (n,c, n*c)
+
+# outro jeito de fazer a tabuada, usando um contador para o multiplicador:
+tab = input("Digite um número para ver sua tabuada: ")
+tab = int(tab)
+cont = 0
+for c in range(1,11): # c igual contador que vai de 1 a 10
+  cont = cont + 1
+  print(f"{tab} x {cont:2} = {tab * cont}")
--- exe50.py
+++ exe50.py
@@ -6,9 +6,22 @@ for c in range(1,7): # c vai ser a variável no intervalo de 1 a 6.
     if n % 2 == 0: #se n for divisível por 2 ...
         soma = soma + n # vai somar só o que foi pedido no if ( está dentro da identação)
         cont = cont + 1 # vai contar só o que foi pedido no if ( está dentro da identação)
-if cont == 1 or cont == 0:
-    print("Você informou {} número PAR e a soma dos números pares é {}." .format(cont,soma))
+if cont == 1:
+    print(f"Você informou {cont} número PAR, logo a soma é {soma}.")
+elif cont == 0:
+    print("Você não informou nenhum número PAR.")
 else:
-    print("Você informou {} números PARES e a soma dos números pares é {}." .format(cont,soma)) #print nesse caso tem que ficar do lado de fora senão ele vai entrar no laço assim que cumprir o que foi comandado.
+    print(f"Você informou {cont} números PARES e a soma dos números pares é {soma}.") #print nesse caso tem que ficar do lado de fora senão ele vai entrar no laço assim que cumprir o que foi comandado.
         
-
+#outra forma de fazer o mesmo exercício, usando o continue para pular os números ímpares:
+print("Soma dos pares.")
+soma = 0
+cont = 0
+for c in range(1,7):
+  num = int(input(f'Digite o {c}° valor: '))
+  if num % 2 == 0:
+    cont = cont + 1 
+    soma = soma + num
+  else:
+    continue
+print(f'Você informou {cont} números PARES e a soma deles é {soma}')
