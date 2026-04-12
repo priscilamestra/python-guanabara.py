01a5764949943ed7a812db4443a59ef0d7a49ee3
priscilamestra <priscilamestra@hotmail.com>
2026-04-12 14:03:02 -0300

    V2 + Formatação atualizada

--- exe56F.py
+++ exe56F.py
@@ -5,7 +5,7 @@ maior_id_homem = 0
 nome_velho = "" #quando a resposta for uma str tem que usar o "" ao invés de 0
 mulher_20 = 0
 for i in range(1,4): #reptir as informações do laço dentro do intervalo solicitado
-    print("----- {}° PESSOA -----" .format(i))
+    print(f"----- {i}° PESSOA -----")
     nome = input("Nome:").strip().upper() #strip = tirar os espaços #upper = caixa alta
     idade = int(input("Idade:"))
     sexo = input("Sexo [M/F]:").upper().strip()
@@ -23,10 +23,84 @@ if mulher_20 == 1:
 if mulher_20 == 0:
     print("Nenhuma mulher tem menos de 20 anos")
 if mulher_20 > 1:
-    print("Ao todo são {} mulheres com menos de 20 anos." .format(mulher_20))
+    print(f"Ao todo são {mulher_20} mulheres com menos de 20 anos.")
 media = soma / i 
-print("A média das idades analisadas é de {:.2f}." .format(media))
-print("O homem mais velho é {} com {} anos." .format(nome_velho,maior_id_homem))
+print(f"A média das idades analisadas é de {media:.2f}.")
+print(f"O homem mais velho é {nome_velho} com {maior_id_homem} anos.")
 
+
+# EXERCÍCIO 56 - ANALISADOR COMPLETO (VERSÃO 3)
+
+soma_idade = 0
+soma_pessoas = 0
+idade_homem_mais_velho = 0
+homem_mais_velho = ''
+total_homens = 0 # A "chave" para a melhor prática
+mulheres_abaixo_20 = 0
+for pessoa in range(1,5):
+  print("-----------")
+  nome = str(input("Nome: ")).strip()
+  idade = int(input("Idade: "))
+  sexo = str(input("Sexo [M/F]: ")).strip().upper()
+  
+  soma_idade = soma_idade + idade
+  soma_pessoas = soma_pessoas + 1
+  
+  # LÓGICA DO HOMEM MAIS VELHO
+  if sexo == 'M':
+    total_homens = total_homens + 1
+		 
+    if total_homens == 1: # Se for o PRIMEIRO HOMEM (independente de ser a pessoa 1 ou 4)
+        idade_homem_mais_velho = idade
+        homem_mais_velho = nome
+    else:
+      if idade > idade_homem_mais_velho: # Se não for o primeiro, eu comparo
+          idade_homem_mais_velho = idade
+          homem_mais_velho = nome
+        
+  if sexo in 'Ff' and idade < 20:
+    mulheres_abaixo_20 = mulheres_abaixo_20 + 1
+
+media_idade = soma_idade / soma_pessoas
+if mulheres_abaixo_20 == 1:
+	msg_mulher = 'mulher'
+else:
+	msg_mulher = 'mulheres'
+print(f"A média de idade do grupo é de {media_idade:.2f} anos\nO homem mais velho tem {idade_homem_mais_velho} anos e se chama {homem_mais_velho}\nAo todo tem {mulheres_abaixo_20} {msg_mulher} com menos de 20 anos")
+
+# EXERCÍCIO 56 - ANALISADOR COMPLETO (VERSÃO 4)
+# Diminuir os if's aninhados (melhor prática) - usando o total_homens para inaugurar o homem mais velho
+
+soma_idade = 0
+soma_pessoas = 0
+idade_homem_mais_velho = 0
+homem_mais_velho = ''
+total_homens = 0 # A "chave" para a melhor prática
+mulheres_abaixo_20 = 0
+for pessoa in range(1,5):
+  print("-----------")
+  nome = str(input("Nome: ")).strip()
+  idade = int(input("Idade: "))
+  sexo = str(input("Sexo [M/F]: ")).strip().upper()
+  
+  soma_idade = soma_idade + idade
+  soma_pessoas = soma_pessoas + 1
+  
+  # LÓGICA DO HOMEM MAIS VELHO
+  if sexo == 'M':
+    if total_homens == 1 or idade > idade_homem_mais_velho:
+      idade_homem_mais_velho = idade
+      homem_mais_velho = nome
+    
+  if sexo == 'F' and idade < 20:
+    mulheres_abaixo_20 += 1
+        
+media_idade = soma_idade / soma_pessoas
+
+if mulheres_abaixo_20 == 1:
+	msg_mulher = 'mulher'
+else:
+	msg_mulher = 'mulheres'
+print(f"A média de idade do grupo é de {media_idade} anos\nO homem mais velho tem {idade_homem_mais_velho} anos e se chama {homem_mais_velho}\nAo todo tem {mulheres_abaixo_20} {msg_mulher} com menos de 20 anos")
 
 
