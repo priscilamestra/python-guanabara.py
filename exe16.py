41f0ad583b82f834656320eaf44d55ccd5c7a044
priscilamestra <priscilamestra@hotmail.com>
2026-05-02 11:27:22 -0300

    Formatação atualizada dos exercícios

--- exe16.py
+++ exe16.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 print("Quebrando um número")
 num = float(input("Digite um número:"))
-print("O número {} tem a parte inteira {:.0f}" .format(num,num))
+print(f"O número {num} tem a parte inteira {num:.0f}")
 
 from math import trunc
 num1 = float(input("Digite um número:"))
-print("O número {} tem a parte inteira {}" .format(num1,trunc(num1)))
+print(f"O número {num1} tem a parte inteira {trunc(num1)}")
 
 num2 = float(input("Digite um número:"))
-print("O número {} tem a parte inteira {}" .format(num2, int(num2)))
-
+print(f"O número {num2} tem a parte inteira {int(num2)}")
