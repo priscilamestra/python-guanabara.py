2ec5edc196179d523c500e2f0b00f139516ba575
priscilamestra <priscilamestra@hotmail.com>
2026-08-27 17:53:16 -0300

    Analisador de Textos

--- Desafios/exe22.py
+++ Desafios/exe22.py
@@ -1,15 +1,14 @@
-# -*- coding: utf-8 -*-
 print("Analisando texto")
 
 nome = input("Digite seu nome completo:").strip()
 print("------- Analisando seu nome -------")
-print("Seu nome em maiúsculo é {}." .format(nome.upper()))
-print("Seu nome em minúsculo é {}.".format(nome.lower()))
-print("Seu nome tem ao todo {} letras." .format(len(nome.replace(" ",""))))
-print("Seu primeiro nome é {} e ele tem {} letras." .format(nome.split()[0],len(nome.split()[0])))
+print(f"Seu nome em maiúsculo é {nome.upper()}.")
+print(f"Seu nome em minúsculo é {nome.lower()}.")
+print(f"Seu nome tem ao todo {nome.replace(" ", "")} letras.")
+print(f"Seu primeiro nome é {nome.split()[0]} e ele tem {len(nome.split([0]))} letras.")
 #print("Seu primeiro nome tem {} letras." .format(nome.find(" "))) 
 nome1 = ("priscila mestra canto pereira")
 print(nome1.find("tra")) 
-print("Seu primeiro nome tem {} letras." .format(nome1.find(" "))) #find conta a posição do espaço, começa em 0.
+print(f"Seu primeiro nome tem {nome1.find(" ")} letras.") #find conta a posição do espaço, começa em 0.
 
 
