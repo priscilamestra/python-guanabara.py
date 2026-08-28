704d51ba7005d2b133f5f2608c2c96c338c2e881
priscilamestra <priscilamestra@hotmail.com>
2026-08-28 14:07:07 -0300

    Procurando uma string dentro de outra

--- Desafios/exe25.py
+++ Desafios/exe25.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 print("Procurando uma string dentro de outra.")
 
 nome = input("Qual seu nome completo? ").strip()
-print("Seu nome tem silva? {}" .format("SILVA" in nome.upper().split())) 
+print(f"Seu nome tem silva? {"SILVA" in nome.upper().split()}")
 
 curso = input("Qual seu curso? ").strip()
-print("Você cursa alguma engenharia? {}" .format("ENGENHARIA" in curso.upper().split()))
+print(f"Você cursa alguma engenharia? {"ENGENHARIA" in curso.upper().split()}")
 
 
 email = input("Qual seu email? ").strip()
